3d3b1487d5f5ca862336bb9b891bab4b57be15d8
Jaime Santos <50669048+JaimePSantos@users.noreply.github.com>
2021-05-24 21:59:21 +0100

    depecrated files

---
+++ Parser/Deprecated/shell2.py
@@ -0,0 +1,12 @@
+import basic2
+
+while True:
+    text = input('basic > ')
+    result, error = basic2.run('<stdin>', text)
+
+    if text == "quit": 
+        print("Exiting...") 
+        break
+
+    if error: print(error.as_string())
+    else: print(result)
